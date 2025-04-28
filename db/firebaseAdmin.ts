ef39da90d29454dd761eed1675c60c8cf23a9465
Iliyas1996 <ilyas1996oktl@gmail.com>
2025-04-28 19:13:01 +0500

    Add files via upload

---
+++ db/firebaseAdmin.ts
@@ -0,0 +1,16 @@
+import admin from "firebase-admin";
+
+interface FirebaseAdminAppParams {
+  projectId: string;
+  clientEmail: string;
+  storageBucket: string;
+  privateKey: string;
+}
+
+function formatPrivateKey(key: string) {
+  return key.replace(/\\n/g, "\n");
+}
+
+export function createFirebaseAdminApp(params: FirebaseAdminAppParams) {
+  const privateKey = formatPrivateKey(params.privateKey);
+}
